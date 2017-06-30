40c3bb523a0445622bbe22e8da6d1d22da228b82
NamelessGhoul0 <namelessghoul02@gmail.com>
2017-06-30 13:40:46 -0400

    removed warning image, replaced with pgf text

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #define EXIT_COMBO (SCE_CTRL_START | SCE_CTRL_SELECT)
 
 #define BLACK   RGBA8(  0,   0,   0, 255)
+#define WHITE   RGBA8(255, 255, 255, 255)
 #define RED     RGBA8(255,   0,   0, 255)
 #define ORANGE  RGBA8(255, 165,   0, 255)
 #define YELLOW  RGBA8(255, 255,   0, 255)
@@ -13,78 +14,92 @@
 #define BLUE    RGBA8(  0,   0, 255, 255)
 #define PURPLE  RGBA8(128,   0, 128, 255)
 
-extern unsigned char warning_image[];
+SceCtrlData pad;
 
-int main()
-{
-    vita2d_init();
-	vita2d_set_clear_color(BLACK);
+int display_warning() {
+	vita2d_pgf *pgf;
+	pgf = vita2d_load_default_pgf();
 
-    vita2d_start_drawing();
-    vita2d_clear_screen();
-    vita2d_texture *warning = vita2d_load_PNG_buffer(warning_image);
-    vita2d_draw_texture(warning, 0, 0);
-    vita2d_end_drawing();
-    vita2d_swap_buffers();
+	while (1) {
+		sceCtrlPeekBufferPositive(0, &pad, 1);
 
-    sceKernelDelayThread(10 * 1000 * 1000); // Delay 10 seconds
+		vita2d_start_drawing();
+		vita2d_clear_screen();
 
-    SceCtrlData pad;
+		vita2d_pgf_draw_text(pgf, 30, 30, RED, 1.0f, "WARNING: This application flashes colors in an attempt to fix dead pixels");
+		vita2d_pgf_draw_text(pgf, 30, 30+30, RED, 1.0f, "If you suffer from photosensitive epilepsy, please look away.");
+		vita2d_pgf_draw_text(pgf, 30, 30+30+30, WHITE, 1.0f, "Press X to continue.");
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
 
-    while(1) {
-		sceKernelPowerTick(0);
+		if (pad.buttons == SCE_CTRL_CROSS) {
+			sceKernelDelayThread(3000000); // Delay for 3 seconds
+			vita2d_free_pgf(pgf);
+			break;
+		}
+	}
 
-        sceCtrlPeekBufferPositive(0, &pad, 1);
+	return 0;
+}
 
-        if (pad.buttons == EXIT_COMBO) { break; }
+int main() {
+	vita2d_init();
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
-        vita2d_draw_rectangle(0, 0, 960, 544, RED);
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
+	display_warning();
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
-        vita2d_draw_rectangle(0, 0, 960, 544, ORANGE);
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
+	while(1) {
+		sceKernelPowerTick(0); // prevent screen from dimming
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
-        vita2d_draw_rectangle(0, 0, 960, 544, YELLOW);
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
+		sceCtrlPeekBufferPositive(0, &pad, 1);
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
-        vita2d_draw_rectangle(0, 0, 960, 544, GREEN);
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
+		if (pad.buttons == EXIT_COMBO) { break; }
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
-        vita2d_draw_rectangle(0, 0, 960, 544, CYAN);
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+		vita2d_draw_rectangle(0, 0, 960, 544, RED);
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
-        vita2d_draw_rectangle(0, 0, 960, 544, BLUE);
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+		vita2d_draw_rectangle(0, 0, 960, 544, ORANGE);
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
 
-        vita2d_start_drawing();
-        vita2d_clear_screen();
-        vita2d_draw_rectangle(0, 0, 960, 544, PURPLE);
-        vita2d_end_drawing();
-        vita2d_swap_buffers();
-    }
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+		vita2d_draw_rectangle(0, 0, 960, 544, YELLOW);
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
 
-    vita2d_fini();
-    vita2d_free_texture(warning);
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+		vita2d_draw_rectangle(0, 0, 960, 544, GREEN);
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
+
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+		vita2d_draw_rectangle(0, 0, 960, 544, CYAN);
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
+
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+		vita2d_draw_rectangle(0, 0, 960, 544, BLUE);
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
+
+		vita2d_start_drawing();
+		vita2d_clear_screen();
+		vita2d_draw_rectangle(0, 0, 960, 544, PURPLE);
+		vita2d_end_drawing();
+		vita2d_swap_buffers();
+	}
+
+	vita2d_fini();
 
 	sceKernelExitProcess(0);
-
-    return 0;
+	
+	return 0;
 }
